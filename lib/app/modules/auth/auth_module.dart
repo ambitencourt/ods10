4b4d9ef709e32d69836f3399961103bd5a450108
pedroenrre <pedro_enrre@hotmail.com>
2022-04-14 14:49:09 -0300

    general adjustes

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:ods10/app/modules/auth/presentation/onboarding/splash_page.dart';
+import 'package:ods10/app/modules/auth/presentation/pages/splash_page.dart';
 
 class AuthModule extends Module {
   @override
