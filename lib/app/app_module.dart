b9227cc1d9eb70bf76188fc4f89030fa1dcbbb04
pedroenrre <pedro_enrre@hotmail.com>
2022-04-15 16:57:45 -0300

    adds injection of user store on app module

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,16 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:ods10/app/api.dart';
+import 'package:ods10/app/common/stores/user_store.dart';
 import 'package:ods10/app/modules/auth/auth_module.dart';
 import 'common/controllers/bottom_navibar_controller.dart';
+import 'common/stores/bottom_navibar_store.dart';
 import 'modules/journey/journey_module.dart';
 import 'modules/profile/profile_module.dart';
-import 'stores/bottom_navibar_store.dart';
 
 class AppModule extends Module {
   @override
   List<Bind> get binds => [
         Bind.singleton<Dio>((i) => api),
+        Bind.singleton((i) => UserStore()),
         Bind.singleton((i) => BottomNavibarStore()),
         Bind.factory((i) => BottomNavibarController(i())),
       ];
