9f667ef36951e2748afa27d8758c232594745b60
pedroenrre <pedro_enrre@hotmail.com>
2022-04-17 14:58:53 -0300

    adds document status tab

---
+++ lib/app/modules/journey/presentation/widgets/documents_status_tab.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:ods10/app/common/resources/app_text_styles.dart';
+import 'package:ods10/app/modules/journey/presentation/widgets/personal_tabs.dart';
+
+class DocumentsStatusTab extends StatelessWidget {
+  late TabController _tabController;
+  DocumentsStatusTab({
+    Key? key,
+    required tabController,
+  }) : super(key: key) {
+    _tabController = tabController;
+  }
+
+  final List<Widget> tabMenu = [
+    const Tab(
+      text: "Todos",
+    ),
+    const Tab(
+      text: "Pendentes",
+    ),
+    const Tab(
+      text: "Solicitados",
+    ),
+    const Tab(
+      text: "Recebidos",
+    ),
+    const Tab(
+      text: "gratuitos",
+    ),
+    const Tab(
+      text: "Pagos",
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 20),
+        RichText(
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: 'Lista de ',
+                style: getRegularStyle(fontSize: 18),
+              ),
+              TextSpan(
+                text: 'Documentos',
+                style: getBoldStyle(fontSize: 18),
+              ),
+            ],
+          ),
+        ),
+        PersonalTabs(
+          tabMenu: tabMenu,
+          onTabSelect: (index) {},
+          tabController: _tabController,
+        ),
+      ],
+    );
+  }
+}
