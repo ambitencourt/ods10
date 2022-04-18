ff9033bc6992c8036812636670bfc83cec3cfe78
Adriano Miranda Bitencourt <73924078+ambitencourt@users.noreply.github.com>
2022-04-17 22:26:10 -0300

    Reparo de arquivo

--- lib/app/modules/journey/presentation/pages/islands/islands_page.dart
+++ lib/app/modules/journey/presentation/pages/islands/islands_page.dart
@@ -85,94 +85,6 @@ class _IslandsPageState
                                 size: 24,
                                 color: Colors.white,
                               ),
-<<<<<<< HEAD
-<<<<<<< HEAD
-                              onTap: () =>
-                                  Modular.to.pushNamed('/journey/documents')),
-                        ],
-                      ),
-                      customSizedBox4(context),
-                      SizedBox(
-                        height: 27,
-                        width: 157,
-                        child: Text(
-                          'Boas vindas ao',
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.dmSerifDisplay(
-                            textStyle: const TextStyle(
-                                fontSize: 20,
-                                fontStyle: FontStyle.italic,
-                                color: Color(0xFF256380)),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 335,
-                        height: 49,
-                        child: Text(
-                          'Oceanótico',
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.dmSerifDisplay(
-                            textStyle: const TextStyle(
-                                fontSize: 36, fontStyle: FontStyle.normal),
-                          ),
-                        ),
-                      ),
-                      customSizedBox3(context),
-                      SizedBox(
-                        width: 335,
-                        height: 36,
-                        child: Text(
-                          'Arraste para o lado ou veja a lista completa de ilhas. Selecione uma das ilhas e inicie o processo.',
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.mulish(
-                            textStyle: const TextStyle(
-                                fontSize: 14, fontStyle: FontStyle.normal),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Stack(
-                  alignment: const Alignment(0, -0.35),
-                  children: [
-                    //Aqui foi utilizado o Package carousel_slider, podendo fazer manualmente no futuro
-                    Hero(
-                      tag: 'hero-island-page',
-                      child: CarouselSlider(
-                        items: imageSlidersIsland,
-                        options: CarouselOptions(
-                          enlargeStrategy: CenterPageEnlargeStrategy.scale,
-                          enlargeCenterPage: false,
-                          enableInfiniteScroll: false,
-                          viewportFraction: 1,
-                          aspectRatio: 96 / 65,
-                          onPageChanged: (index, reason) {
-                            setState(() {
-                              current = index;
-                            });
-                          },
-                        ),
-                        carouselController: _controller,
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        InkWell(
-                          borderRadius: BorderRadius.circular(220),
-                          child: current == 0
-                              ? Container(
-=======
-                              onTap: () => Modular.to.pushNamed('/journey'),
-                            ),
-                            const Spacer(),
-                            InkWell(
-                                borderRadius: BorderRadius.circular(220),
-                                child: Container(
->>>>>>> de79ec1a23b4b9873f62a77216bccbc11674196c
-=======
                             ),
                             onTap: () =>
                                 Modular.to.pushNamed('/journey/documents'),
@@ -256,7 +168,6 @@ class _IslandsPageState
                           borderRadius: BorderRadius.circular(220),
                           child: controller.islandsPageStore.current == 0
                               ? Container(
->>>>>>> 3765474ec2e8e4efd0c2cd0daa04b7d09e451375
                                   width: 60,
                                   height: 60,
                                   decoration: const BoxDecoration(
@@ -325,10 +236,8 @@ class _IslandsPageState
                                       color: Colors.white,
                                     ),
                                   ),
-<<<<<<< HEAD
-<<<<<<< HEAD
                             onTap: () {
-                              _controller.nextPage();
+                              controller.carouselController.nextPage();
                             }),
                       ],
                     ),
@@ -336,8 +245,8 @@ class _IslandsPageState
                     Positioned(
                       top: 150,
                       child: Visibility(
-                        visible: current > 0,
-                        child: current != 4
+                        visible: controller.islandsPageStore.current > 0,
+                        child: controller.islandsPageStore.current != 4
                             ? Container(
                                 decoration: const BoxDecoration(
                                   shape: BoxShape.rectangle,
@@ -474,161 +383,6 @@ class _IslandsPageState
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: imgListIsland.asMap().entries.map((entry) {
                       return GestureDetector(
-                        onTap: () => _controller.animateToPage(entry.key),
-                        child: Container(
-                          width: 12.0,
-                          height: 12.0,
-                          margin: const EdgeInsets.symmetric(
-                              vertical: 2.0, horizontal: 4.0),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: current == entry.key
-                                ? const Color(0xFFD03363)
-                                : const Color(0xFFB8B8B8),
-=======
-                            onTap: () =>
-                                controller.carouselController.previousPage(),
-                          ),
-                          SizedBox(
-                            width: mediaQuery.width * .5,
-                          ),
-                          InkWell(
-                              borderRadius: BorderRadius.circular(220),
-                              child: controller.islandsPageStore.current == 4
-                                  ? Container(
-                                      width: 60,
-                                      height: 60,
-                                      decoration: const BoxDecoration(
-                                        color:
-                                            Color.fromARGB(20, 255, 255, 255),
-                                        shape: BoxShape.circle,
-                                      ),
-                                      child: const Icon(
-                                        Icons.arrow_forward,
-                                        size: 24,
-                                        color: Colors.black,
-                                      ),
-                                    )
-                                  : Container(
-                                      width: 60,
-                                      height: 60,
-                                      decoration: BoxDecoration(
-                                        border: Border.all(
-                                          color: Colors.white,
-                                          width: 2,
-                                        ),
-                                        color: const Color(0xFF256380),
-                                        shape: BoxShape.circle,
-                                      ),
-                                      child: const Icon(
-                                        Icons.arrow_forward,
-                                        size: 24,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                              onTap: () {
-                                controller.carouselController.nextPage();
-                              }),
-                        ],
-                      ),
-                      Align(
-                        alignment: const Alignment(0, 0),
-                        child: Visibility(
-                          visible: false,
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.rectangle,
-                              color: Colors.white,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(38),
-                              ),
-=======
-                            onTap: () {
-                              controller.carouselController.nextPage();
-                            }),
-                      ],
-                    ),
-                    Align(
-                      alignment: const Alignment(0, 0),
-                      child: Visibility(
-                        visible: false,
-                        child: Container(
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.rectangle,
-                            color: Colors.white,
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(38),
->>>>>>> 3765474ec2e8e4efd0c2cd0daa04b7d09e451375
-                            ),
-                          ),
-                          height: 62,
-                          width: 187,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Container(
-                                width: 60,
-                                height: 60,
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: const Color(0xFFD03363),
-                                    width: 3,
-                                  ),
-                                  color: Colors.white,
-                                  shape: BoxShape.circle,
-                                ),
-                                child: const Icon(
-                                  Icons.play_arrow,
-                                  size: 30,
-                                  color: Color(0xFFD03363),
-                                ),
-                              ),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'Entrar na ilha',
-                                    style: GoogleFonts.mulish(
-                                      textStyle: const TextStyle(
-                                        fontSize: 12,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w700,
-                                      ),
-                                    ),
-                                  ),
-                                  Text(
-                                    'João Nery',
-                                    style: GoogleFonts.dmSerifDisplay(
-                                      textStyle: const TextStyle(
-                                        fontSize: 20,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w700,
-                                      ),
-                                    ),
-<<<<<<< HEAD
-                                  ],
-                                ),
-                              ],
-                            ),
->>>>>>> de79ec1a23b4b9873f62a77216bccbc11674196c
-=======
-                                  ),
-                                ],
-                              ),
-                            ],
->>>>>>> 3765474ec2e8e4efd0c2cd0daa04b7d09e451375
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: imgListIsland.asMap().entries.map((entry) {
-                      return GestureDetector(
                         onTap: () => controller.carouselController
                             .animateToPage(entry.key),
                         child: Container(
@@ -668,7 +422,7 @@ class _IslandsPageState
                         child: LinearProgressIndicator(
                           minHeight: 5,
                           backgroundColor: const Color(0xFFD2D2CC),
-                          value: controller.islandsPageStore.progressValue / 30,
+                          value: controller.islandsPageStore.progressValue / 22,
                           valueColor: const AlwaysStoppedAnimation<Color>(
                               Color(0xFFD03363)),
                         ),
@@ -691,7 +445,7 @@ class _IslandsPageState
                         ),
                         const Spacer(),
                         Text(
-                          '${controller.islandsPageStore.progressValue.round()}/30 documentos',
+                          '${controller.islandsPageStore.progressValue.round()}/22 documentos',
                           style: GoogleFonts.mulish(
                               textStyle: const TextStyle(fontSize: 10),
                               fontWeight: FontWeight.w600,
